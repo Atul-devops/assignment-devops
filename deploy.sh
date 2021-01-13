528c148d58a4bc82d4b8678f37d5518f9a6b5189
ATUL <62421988+Atul-devops@users.noreply.github.com>
2021-01-13 19:46:03 +0530

    Create deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ terraform apply -auto-approve
 
 cd ../ansible
 # pull the instance information from Terraform, and run the Ansible playbook against it to configure
-TF_STATE=../terraform/terraform.tfstate ansible-playbook "--inventory-file=$(which terraform-inventory)" windows.yml
+TF_STATE=../Terraform/terraform.tfstate ansible-playbook "--inventory-file=$(which terraform-inventory)" provision.yml
 
 echo "Success!"
