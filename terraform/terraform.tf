e726f6f1bfc558c7d62e233ab006f4e5c0f7579c
ATUL <62421988+Atul-devops@users.noreply.github.com>
2021-01-14 20:22:46 +0530

    Create terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,8 +1,11 @@
-provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-    region = "${var.aws_region}"
+terraform {
+  backend "s3" {
+    bucket = "octopus-terraform-releasetour"
+    key    = "releasetour.tfstate"
+    region = "region"
+  }
 }
+
 
 resource "aws_instance" "windows-instance" {
   ami = "ami-e3a2f79f"
